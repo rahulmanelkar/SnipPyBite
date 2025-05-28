1636473a106b73eaab8e93dc88dcf674bb34621e
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-05-27 21:43:31 -0700

    added InMemorySnippetRepo

--- src/snippybite/repo.py
+++ src/snippybite/repo.py
@@ -19,3 +19,21 @@ class SnippetRepository(ABC):
     @abstractmethod
     def delete(self, snippet_id: int) -> None:
         pass
+
+
+class InMemorySnippetRepo(SnippetRepository):
+    def __init__(self):
+        self._snippets: dict[int, Snippet] = {}
+
+    def add(self, snippet: Snippet) -> None:
+        self._snippets[snippet.id] = snippet
+
+    def list(self) -> list[Snippet]:
+        return list(self._snippets.values())
+
+    def get(self, snippet_id: int) -> Snippet | None:
+        return self._snippets.get(snippet_id)
+
+    def delete(self, snippet_id: int) -> None:
+        if snippet_id in self._snippets:
+            del self._snippets[snippet_id]
